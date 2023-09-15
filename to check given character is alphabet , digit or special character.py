31c0977e5ad309d55f5ea262290cd3ad98803595
parthgoyal3388 <143782562+parthgoyal3388@users.noreply.github.com>
2023-09-15 21:29:38 +0530

    Update to check given character is alphabet , digit or special character.py

--- to check given character is alphabet , digit or special character.py
+++ to check given character is alphabet , digit or special character.py
@@ -1,5 +1,5 @@
 ch=input("enter the value:")
-if 'a'<=ch<='z':
+if 'a'<=ch<='z or 'A'<=ch<='Z':
     print("This character ",ch,"is alphabet")
 elif '0'<=ch<='9':
     print("This character",ch,"is digit")
