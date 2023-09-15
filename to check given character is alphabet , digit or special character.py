ddf3f93e53ea1d13a66888be1a6cbc4f29b23e84
parthgoyal3388 <143782562+parthgoyal3388@users.noreply.github.com>
2023-09-15 21:25:09 +0530

    Add files via upload

---
+++ to check given character is alphabet , digit or special character.py
@@ -0,0 +1,7 @@
+ch=input("enter the value:")
+if 'a'<=ch<='z':
+    print("This character ",ch,"is alphabet")
+elif '0'<=ch<='9':
+    print("This character",ch,"is digit")
+else:
+    print("This character",ch,"is special character")
