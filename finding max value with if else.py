cab933c425a8799c78ec3bdb4c6c81cb85f13ee8
parthgoyal3388 <143782562+parthgoyal3388@users.noreply.github.com>
2023-09-15 22:11:57 +0530

    Add files via upload

---
+++ finding max value with if else.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b and a>c:
+    print("greatest no is :",a)
+elif b>a and b>c:
+    print("greatest no is :",b)
+else :
+    print("greatest no is :",c)
